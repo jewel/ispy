b0ebf5f71439056afb2b9c2cccdae32d8bdf29cd
Steven <jewel@bertha.tuxng.com>
2022-04-30 17:48:13 -0600

    Show kid faces if available

--- public/client.js
+++ public/client.js
@@ -3,23 +3,43 @@ document.addEventListener("DOMContentLoaded", function() {
   var images = {};
   var container = document.getElementById("images");
   source.addEventListener("update", function(event) {
-    var names = JSON.parse(event.data);
-    names.forEach( function(name) {
+    var updates = JSON.parse(event.data);
+    for( var [key, value] of Object.entries(images) ) {
+      value.dataset.present = false;
+    }
+    updates.forEach( function(info) {
+      var name = info[0];
+      var time = info[1];
       var image = images[name];
       if( !image ) {
         image = document.createElement('img');
+        image.classList.add("screen");
         images[name] = image;
         var figure = document.createElement('figure');
         figure.appendChild(image);
-        var caption = document.createElement('figcaption')
-        caption.textContent = name;
-        figure.appendChild(caption);
+        var parts = name.split("@");
+        var kidName = parts[0];
+        if( window.kids && kids[kidName] ) {
+          var kid = kids[kidName];
+          var portrait = document.createElement('img');
+          portrait.classList.add("portrait");
+          portrait.src = kid.picture;
+          portrait.style.borderColor = kid.color;
+          figure.appendChild(portrait);
+          image.style.borderColor = kid.color;
+        }
+        else {
+          var caption = document.createElement('figcaption')
+          caption.textContent = name;
+          figure.appendChild(caption);
+        }
         container.appendChild(figure);
       }
-      image.src = "/view/" + name + "?" + Date.now();
+      image.src = "/view/" + name + "?" + time;
+      image.dataset.present = true;
     });
     for( var [key, value] of Object.entries(images) ) {
-      if( names.indexOf( key ) >= 0 )
+      if( value.dataset.present )
         continue;
       delete images[key];
       value.parentNode.remove();
